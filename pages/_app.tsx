75e684e51c7ef0501bc1c672973ef32ad32d0d5d
Yoriki Yamaguchi <yorikiyamaguchi@gmail.com>
2022-05-20 16:14:15 +0900

    implement sample

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN!}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!}
+      redirectUri={process.env.NEXT_PUBLIC_AUTH0_REDIRECT_URI!}
+    >
+      <Component {...pageProps} />
+    </Auth0Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
